e29f653fa93e1d32abec15dd9e384532827dbc14
Gordon Watts <gordon@gordonandpaula.net>
2014-09-24 15:47:01 +0200

    Use GetROOTVersion task.

---
+++ FindInstalledROOTVersion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindInstalledROOTVersion
+{
+    class Program
+    {
+        /// <summary>
+        /// The code is all in the MSBuild csproj file.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
