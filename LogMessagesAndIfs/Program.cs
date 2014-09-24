713bf86e98f6063b718707ba48470563a1cd4ddc
Gordon Watts <gordon@gordonandpaula.net>
2014-09-24 14:48:32 +0200

    Raw pacakge

---
+++ LogMessagesAndIfs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogMessagesAndIfs
+{
+    class Program
+    {
+        /// <summary>
+        /// The real code here is MSBuild:
+        /// - Write out a log message during the build
+        /// - If statement in the code
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
