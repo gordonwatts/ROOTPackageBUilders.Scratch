b996aa52bafc8c312c173375cc7194a30a831981
Gordon Watts <gordon@gordonandpaula.net>
2014-09-25 16:27:39 +0200

    New test to actually use MSBUILD to download ROOT
    In a real C# project. Not running yet...

---
+++ FileAndHistogramTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using ROOTNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAndHistogramTest
+{
+    class Program
+    {
+        /// <summary>
+        /// Simple test to use the MSBuild guys in a special targets file.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var f = NTFile.Open("junk.root", "RECREATE");
+            var h = new NTH1F("hi", "there", 10, 0.0, 1.0);
+            h.Fill(0.5);
+            f.Write();
+            f.Close();
+        }
+    }
+}
