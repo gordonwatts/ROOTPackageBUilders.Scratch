184dd39b9e86113f6debc0726b4273390906e607
Gordon Watts <gordon@gordonandpaula.net>
2014-09-25 06:14:54 +0200

    Testing out the ROOT unpacker, first version

---
+++ DownloadUnpackROOT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadUnpackROOT
+{
+    class Program
+    {
+        /// <summary>
+        /// All the sample code is in the MSBuild csproj code here!
+        /// Run the root download task.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
