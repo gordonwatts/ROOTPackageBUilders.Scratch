f7317e90fd53fac60b4eb39deac063469f3973a8
Gordon Watts <gordon@gordonandpaula.net>
2014-10-31 22:07:12 +0100

    Random testing, no idea how good this check is!

--- FileAndHistogramTest/Program.cs
+++ FileAndHistogramTest/Program.cs
@@ -15,6 +15,12 @@ namespace FileAndHistogramTest
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var vs = Environment.GetEnvironmentVariables();
+            foreach (var v in vs.Keys.Cast<string>().OrderBy(s => s))
+            {
+                Console.WriteLine(v);
+            }
+
             var f = NTFile.Open("junk.root", "RECREATE");
             var h = new NTH1F("hi", "there", 10, 0.0, 1.0);
             h.Fill(0.5);
---
+++ start_ROOTPackageBUilders.Scratch.bat
@@ -0,0 +1,4 @@
+@REM Use to start devenv
+@set ROOTSYS=c:\root
+@set PATH=c:\root\bin;%PATH%
+@D:\Code\ROOTPackageBUilders.Scratch\ROOTPackageBUilders.Scratch.sln
